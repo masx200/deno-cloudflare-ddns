52a6dd54f6c6a939d8642d564282d37294945c33
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-08 09:24:35 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,8 @@
-import { startIpv4DDNS, startIpv6DDNS } from "./cloudflare-ddns.ts";
+import {
+    createStartDDNS,
+    startIpv4DDNS,
+    startIpv6DDNS,
+} from "./cloudflare-ddns.ts";
 import { createOrPatchDNSRecord } from "./createOrPatchDNSRecord.ts";
 import { get_dns_record_id_of_name_type } from "./get_dns_record_id_of_name_type.ts";
 import { get_zone_id_of_name } from "./get_zone_id_of_name.ts";
@@ -6,6 +10,7 @@ import { get_zone_name_from_dns_name } from "./get_zone_name_from_dns_name.ts";
 
 export {
     createOrPatchDNSRecord,
+    createStartDDNS,
     get_dns_record_id_of_name_type,
     get_zone_id_of_name,
     get_zone_name_from_dns_name,
